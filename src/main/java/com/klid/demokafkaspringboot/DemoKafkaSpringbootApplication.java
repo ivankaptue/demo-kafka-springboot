e84f0e33e5db5a93ff8adc2c8b80a16ea5a81253
Ivan Kaptue <ivanokaptue@gmail.com>
2022-05-11 10:35:25 -0400

    Add kafka headers

--- src/main/java/com/klid/demokafkaspringboot/DemoKafkaSpringbootApplication.java
+++ src/main/java/com/klid/demokafkaspringboot/DemoKafkaSpringbootApplication.java
@@ -1,10 +1,15 @@
 package com.klid.demokafkaspringboot;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.time.Instant;
 
 @SpringBootApplication
 public class DemoKafkaSpringbootApplication {
@@ -16,8 +21,20 @@ public class DemoKafkaSpringbootApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
+            var topic = "kafka.springboot.topic";
             for (int i = 0; i < 100; i++) {
-                kafkaTemplate.send("kafka.springboot.topic", "hello kafka :) " + i);
+                var messageText = "hello kafka :) " + i;
+//                var record = new ProducerRecord<String, String>(topic, messageText);
+                var timestampKey = KafkaHeaders.TIMESTAMP;
+
+                System.out.println("timestamp key : " + timestampKey);
+
+                var message = MessageBuilder.withPayload(messageText)
+                        .setHeader(KafkaHeaders.TOPIC, topic)
+                        .setHeader(timestampKey, 123456L)
+                        .setHeader(KafkaHeaders.CORRELATION_ID, String.valueOf(i)).build();
+               var result =  kafkaTemplate.send(message).get();
+                System.out.println("Sent result : " + result);
             }
         };
     }
--- src/main/java/com/klid/demokafkaspringboot/KafkaConsumer.java
+++ src/main/java/com/klid/demokafkaspringboot/KafkaConsumer.java
@@ -1,6 +1,9 @@
 package com.klid.demokafkaspringboot;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,8 +15,14 @@ public class KafkaConsumer {
     @KafkaListener(
             topics = "kafka.springboot.topic",
             groupId = "kafka.springboot.group")
-    void listen(String data) {
+    void listen(
+            @Payload String data,
+            @Header(KafkaHeaders.OFFSET) String offset,
+            @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long timestamp
+            ) {
         System.out.println("Listener received : " + data + " :)");
+        System.out.println("From offset : " + offset + " :)");
+        System.out.println("With timestamp : " + timestamp + " :)");
     }
 
 }
